b38c493beb43ab4cd604b92bc04bdbf2f1e5aeef
Deval-Mehta <deval.mehta1092@gmail.com>
2026-03-23 16:35:07 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/AIM-Neurology-Web-Page/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
